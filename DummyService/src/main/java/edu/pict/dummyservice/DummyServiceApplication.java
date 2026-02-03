3bd1aca4dc74f0af2776a27c38daae902d141d1f
shrihari7396 <shriharik121@gmail.com>
2026-02-03 22:27:28 +0530

    Done

---
+++ DummyService/src/main/java/edu/pict/dummyservice/DummyServiceApplication.java
@@ -0,0 +1,13 @@
+package edu.pict.dummyservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DummyServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DummyServiceApplication.class, args);
+    }
+
+}
