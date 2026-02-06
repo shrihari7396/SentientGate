d1ab2cf20951c88861f1ba8b2562afb62c661d4c
shrihari7396 <shriharik121@gmail.com>
2026-02-06 23:07:23 +0530

    Communication And Service Issuses are Resolved ...

--- LogingService/src/main/java/edu/pict/loggingservice/kafka/consumer/GatewayDecisionConsumer.java
+++ LogingService/src/main/java/edu/pict/loggingservice/kafka/consumer/GatewayDecisionConsumer.java
@@ -4,6 +4,7 @@ import edu.pict.loggingservice.config.KafkaTopics;
 import edu.pict.loggingservice.kafka.model.GatewayDecisionEvent;
 import edu.pict.loggingservice.service.KafkaBatchService;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.stereotype.Component;
 
@@ -11,6 +12,7 @@ import java.util.List;
 
 @Component
 @RequiredArgsConstructor
+@Slf4j
 public class GatewayDecisionConsumer {
 
     private final KafkaBatchService kafkaBatchService;
@@ -20,6 +22,7 @@ public class GatewayDecisionConsumer {
             containerFactory = "kafkaListenerContainerFactory"
     )
     public void consume(List<GatewayDecisionEvent> events) {
+        log.info("Received gateway decision events: " + events);
         kafkaBatchService.consumeBatch(events);
     }
 }
