c6d435a415e8f298ea6b0ae7819bafbcbdd5bd1f
shrihari7396 <shriharik121@gmail.com>
2026-02-13 12:59:59 +0530

    PolicyService Delete

--- PolicyService/src/main/java/edu/pict/policyservice/rules/PolicyRule.java
+++
@@ -1,8 +0,0 @@
-package edu.pict.policyservice.rules;
-
-import edu.pict.policyservice.model.PolicyContext;
-import edu.pict.policyservice.model.RuleResult;
-
-public interface PolicyRule {
-    RuleResult evaluate(PolicyContext context);
-}
