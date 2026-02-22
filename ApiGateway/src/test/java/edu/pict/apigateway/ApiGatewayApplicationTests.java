0c26b8f70451f9493e30932f1caac56ec13d3d9b
shrihari7396 <shriharik121@gmail.com>
2026-02-22 21:06:37 +0530

    Test Files Added to Api Gateway AI Genrated

--- ApiGateway/src/test/java/edu/pict/apigateway/ApiGatewayApplicationTests.java
+++ ApiGateway/src/test/java/edu/pict/apigateway/ApiGatewayApplicationTests.java
@@ -3,7 +3,10 @@ package edu.pict.apigateway;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+@SpringBootTest(properties = {
+        "sentinel.security.secret-key=asbdfhabsdfhnjsndjnjdsncnsadncaiusdhfiuwhe7ryh",
+        "jwt.secret-key=asbdfhabsdfhnjsndjnjdsncnsadncaiusdhfiuwhe7ryhasbdfhabsdfhnjsndjnjdsncnsadncaiusdhfiuwhe7ryhasbdfhabsdfhnjsndjnjdsncnsadncaiusdhfiuwhe7ryhasbdfhabsdfhnjsndjnjdsncnsadncaiusdhfiuwhe7ryhasbdfhabsdfhnjsndjnjdsncnsadncaiusdhfiuwhe7ryhasbdfhabsdfhnjsndjnjdsncnsadncaiusdhfiuwhe7ryhasbdfhabsdfhnjsndjnjdsncnsadncaiusdhfiuwhe7ryhasbdfhabsdfhnjsndjnjdsncnsadncaiusdhfiuwhe7ryhasbdfhabsdfhnjsndjnjdsncnsadncaiusdhfiuwhe7ryhasbdfhabsdfhnjsndjnjdsncnsadncaiusdhfiuwhe7ryhasbdfhabsdfhnjsndjnjdsncnsadncaiusdhfiuwhe7ryhasbdfhabsdfhnjsndjnjdsncnsadncaiusdhfiuwhe7ryh"
+})
 class ApiGatewayApplicationTests {
 
     @Test
