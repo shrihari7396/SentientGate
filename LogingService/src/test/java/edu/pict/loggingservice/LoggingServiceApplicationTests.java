0015b30abd1922b713c8ac14957a1094ec777f1a
shrihari7396 <shriharik121@gmail.com>
2026-02-23 14:12:16 +0530

    Frontend Started

---
+++ LogingService/src/test/java/edu/pict/loggingservice/LoggingServiceApplicationTests.java
@@ -0,0 +1,12 @@
+package edu.pict.loggingservice;
+
+import org.junit.jupiter.api.Test;
+
+class LoggingServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+        // Simple smoke test without full context load
+    }
+
+}
