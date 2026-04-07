8bae05395b413a7e313c30c6cf93822459d01930
shrihari7396 <shriharik121@gmail.com>
2026-04-07 18:45:06 +0530

    Logging Service renamed ...

--- LoggingService/src/main/java/edu/pict/loggingservice/LoggingServiceApplication.java
+++ LoggingService/src/main/java/edu/pict/loggingservice/LoggingServiceApplication.java
@@ -2,7 +2,6 @@ package edu.pict.loggingservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
 import org.springframework.cache.annotation.EnableCaching;
 
 @SpringBootApplication
--- LoggingService/src/main/java/edu/pict/loggingservice/entity/GatewayLogEntity.java
+++ LoggingService/src/main/java/edu/pict/loggingservice/entity/GatewayLogEntity.java
@@ -9,9 +9,9 @@ import lombok.*;
 @Table(
         name = "gateway_logs",
         indexes = {
-                @Index(name = "idx_logs_occurred_at", columnList = "occurred_at"),
-                @Index(name = "idx_logs_visitor_occurred", columnList = "visitor_id, occurred_at"),
-                @Index(name = "idx_logs_ip_occurred", columnList = "client_ip, occurred_at")
+            @Index(name = "idx_logs_occurred_at", columnList = "occurred_at"),
+            @Index(name = "idx_logs_visitor_occurred", columnList = "visitor_id, occurred_at"),
+            @Index(name = "idx_logs_ip_occurred", columnList = "client_ip, occurred_at")
         })
 @Getter
 @Setter
