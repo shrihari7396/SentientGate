d04ace346c2921fef05a9a3a19c43c7ac03dc671
shrihari7396 <shriharik121@gmail.com>
2026-05-13 17:25:24 +0530

    Fixed Logical Issues ...

--- ApiGateway/src/main/java/edu/pict/apigateway/filters/global/BlacklistFilter.java
+++ ApiGateway/src/main/java/edu/pict/apigateway/filters/global/BlacklistFilter.java
@@ -34,7 +34,8 @@ public class BlacklistFilter implements GlobalFilter, Ordered {
                 .flatMap(
                         isBlocked -> {
                             if (Boolean.TRUE.equals(isBlocked)) {
-                                log.warn("Request blocked: UUID {} is on the blacklist.", uuid);
+//                                For Performance reason log is commented
+//                                log.warn("Request blocked: UUID {} is on the blacklist.", uuid);
                                 exchange.getResponse().setStatusCode(HttpStatus.FORBIDDEN);
                                 return exchange.getResponse().setComplete();
                             }
