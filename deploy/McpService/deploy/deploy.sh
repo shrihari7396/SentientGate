18a904dcd1282585c188c3a31fb9aecda0c1290f
shrihari7396 <shriharik121@gmail.com>
2026-07-08 21:00:02 +0530

    Kubernates Deployment For MCPService  ...

--- deploy/McpService/deploy/deploy.sh
+++ deploy/McpService/deploy/deploy.sh
@@ -3,11 +3,9 @@
 set -e
 # Retrieve the directory path of the active script to ensure relative paths resolve correctly
 SCRIPT_DIR="$(dirname "$(readlink -f "$0")")"
-# Apply the Secret resources to initialize database credentials in the cluster
-kubectl apply -f "${SCRIPT_DIR}/secret.yml"
-# Apply the ConfigMap configurations to initialize logging service settings in the cluster
+# Apply the ConfigMap configurations to initialize MCP settings in the cluster
 kubectl apply -f "${SCRIPT_DIR}/config.yml"
-# Apply the Deployment and Service configurations to run and expose the Logging Service container
+# Apply the Deployment and Service configurations to run and expose the MCP containers
 kubectl apply -f "${SCRIPT_DIR}/deployment.yml"
-# Apply the HorizontalPodAutoscaler to enable dynamic autoscaling for the Logging Service
+# Apply the HorizontalPodAutoscaler to enable dynamic autoscaling for the MCP service
 kubectl apply -f "${SCRIPT_DIR}/hpa.yml"
